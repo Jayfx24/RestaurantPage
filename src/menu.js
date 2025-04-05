4efb293046524f645b304f62af9f0306e1219759
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2025-04-05 13:55:14 -0400

    Updating UI

--- src/menu.js
+++ src/menu.js
@@ -11,44 +11,43 @@ class Menu {
 menuArray.push(
   new Menu(
     "testFood",
-    "lorem testing tester",
+    "Paella with shrimp, mussels, pork, chicken, saffron rice, peas, and bell peppers.",
     "30.99",
     "https://www.thespruceeats.com/thmb/AQ5diUdXp9a9bXJMwtSTtG0lpr8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/SeafoodPaellaWithPorkandChicken-Westend61_GettyImages-b0e7019672484c8a91c250d399d1d163.jpg"
   ),
   new Menu(
     "Crispy Fried Oysters With Cornmeal Batter",
-    "Oysters Rockefeller may be more widely known, but crispy fried oysters are the Southern twist everyone has come to love. Serve them as a meal or on top of a salad for equal parts salty and savory with a cornmeal breading. They go well with tartar sauce or classic rémoulade.",
+    "Fresh oysters, cornmeal batter, eggs, buttermilk, flour, and spices. Served with tartar sauce or rémoulade.",
     "44.99",
     "https://www.thespruceeats.com/thmb/TSY9sgPUTIRHw73LH5lmcWZAipA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/fried-oysters-with-cornmeal-batter-3058714-hero-01-72bfb78fb3f945108b40c7b612c60d93.jpg"
   ),
   new Menu(
     "Shrimp Ceviche",
-    'Delight your dinner guests with this refreshing, no-cook appetizer that is a staple of seaside resorts. It is essentially a low-calorie shrimp cocktail that marinates or "cures" raw shrimp in citrus juices, with diced raw veggies, chiles, and herbs for flavoring. Buy the freshest shrimp you can find for this yummy starter.',
+    "Shrimp cured in lime juice, mixed with tomatoes, onions, cucumbers, cilantro, avocado, and chili peppers.",
     "64.99",
     "https://www.thespruceeats.com/thmb/7o0pqbOlZoVfrd5QrOqdrb0w8iU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/shrimp-ceviche-recipe-5213319-hero-01-8d349ee0591048a588ffac66b3c5870c.jpg"
-
   ),
   new Menu(
     'Beef Wellington',
-    "If you ever watched Hell's Kitchen, you may feel intimidated to make beef Wellington, a stunning dish of juicy tenderloin wrapped in mushroom duxelles, and encased in a puff pastry crust. But there is no need — this genius recipe walks you through each step of creating Gordon Ramsay's most famous restaurant dish in your kitchen without any of the drama.",
+    "Tender beef fillet, mushroom duxelles, Parma ham, and puff pastry. Served with red wine reduction.",
     '59.99',
     'https://www.thespruceeats.com/thmb/H1VKmdNz66CPosoP3cwLXU5RtTk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/make-a-classic-beef-wellington-easy-435726-hero-03-1210c31e884044bcb9c1d1475ca818c3.jpg'
   ),
   new Menu(
     'Steak au Poivre',
-    "A classic in every French cookbook, steak au poivre is an excellent main dish for a romantic meal. It is a fancy meal that comes together in a single skillet with perfectly pan-seared, peppercorn-crusted steaks. It's coated in a rich and creamy blanket of cognac sauce. Serve it with truffle fries or chateau potatoes",
+    "Peppercorn-crusted steak, seared and topped with a creamy cognac and beef stock reduction sauce.",
     '129.99',
     'https://www.thespruceeats.com/thmb/9_J6fGTFQz5sQNucdwvFLGtGd2w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ClassicFrenchSteakauPoivre-TheSpruce-5c62efe3c9e77c0001662762.jpg'
-
   ),
   new Menu(
     'Grilled Filet Mignon with Bearnaise Sauce',
-    'There are a few French sauces that you can master to take all of your dishes to the next culinary level. Learn how to make this insanely delicious classic French Bearnaise sauce for topping tender filet mignons and dipping steamed asparagus spears. You will have a go-to magic trick for every special dinner in your future.',
+    "Grilled filet mignon, topped with Bearnaise sauce made of egg yolks, butter, vinegar, shallots, and tarragon.",
     '69.99',
     'https://www.thespruceeats.com/thmb/5fYyZdcgUtV_wYmp8OACRXO-x2o=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/grilled-filet-mignon-with-bearnaise-sauce-3573497-hero-01-9b0abe87c0524a799724135b99b483c8.jpg'
-
   )
 );
+
+
 
 export function createMenuList() {
   const contentContainer = document.querySelector("#content");
@@ -106,8 +105,8 @@ function boxContent() {
 
 
     boxTextItems.appendChild(boxText)
-    boxTextItems.appendChild(price)
     boxTextItems.appendChild(boxDescription)
+    boxTextItems.appendChild(price)
     box.appendChild(img)
     box.appendChild(boxTextItems)
     

9c9ae50b922e49d591ce488eef25cc6808aee488
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2025-04-09 14:07:12 -0400

    Updating Reserve

--- src/reserve.js
+++ src/reserve.js
@@ -8,8 +8,7 @@ reserveContainer.id = 'reserveContainer';
 
 export function createReserve(){
 
-   
-    
+
     contentContainer.innerHTML = ''
     reserveContainer.appendChild(reserveForm)
     contentContainer.appendChild(reserveContainer)
@@ -20,17 +19,19 @@ export function createReserve(){
 function createForm(){
     reserveForm.id = 'reserveForm';
 
-    const nameLabel = document.createElement('label')
-    const nameInput = document.createElement('input')
-    const nameDiv = document.createElement('div')
-    const dateDiv = document.createElement('div')
-    const emailDiv = document.createElement('div')
-    const peopleDiv = document.createElement('div')
-    const requestsDiv = document.createElement('div')
+    const nameLabel = document.createElement('label');
+    const nameInput = document.createElement('input');
+    const nameDiv = document.createElement('div');
+    const dateDiv = document.createElement('div');
+    const emailDiv = document.createElement('div');
+    const peopleDiv = document.createElement('div');
+    const requestsDiv = document.createElement('div');
+    const timeDiv = document.createElement('div');
 
 
     nameDiv.classList = 'form-group'
     dateDiv.classList = 'form-group'
+    timeDiv.classList = 'form-group'
     emailDiv.classList = 'form-group'
     peopleDiv.classList = 'form-group'
     requestsDiv.classList = 'form-group'
@@ -55,7 +56,7 @@ function createForm(){
     const dateInput = document.createElement('input')
     dateLabel.textContent= "Reservation Date "
     dateLabel.setAttribute("for", "date")
-    dateInput.setAttribute("type", "datetime-local");
+    dateInput.setAttribute("type", "date");
     dateInput.setAttribute("id", "date");
 
     dateInput.setAttribute("name", "date");
@@ -65,12 +66,24 @@ function createForm(){
     dateDiv.appendChild(dateInput)
     reserveForm.appendChild(dateDiv);
 
+    const timeLabel = document.createElement('label')
+    const timeInput = document.createElement('input')
+    timeLabel.textContent= "Reservation Time "
+    timeLabel.setAttribute("for", "time");
+    timeInput.setAttribute("type", "time");
+    timeInput.setAttribute("id", "time");
+    timeInput.setAttribute("name", "time");
+    timeInput.setAttribute("required", "true");
+
+    timeDiv.appendChild(timeLabel)
+    timeDiv.appendChild(timeInput)
+    reserveForm.appendChild(timeDiv);
 
 
     const emailLabel = document.createElement('label')
     const emailInput = document.createElement('input')
     emailLabel.textContent= "Email "
-    emailLabel.setAttribute("for", "time")
+    emailLabel.setAttribute("for", "email")
     emailInput.setAttribute("type", "email");
     emailInput.setAttribute("name", "email");
     emailInput.setAttribute("id", "email");
@@ -119,15 +132,16 @@ function createForm(){
 
 }
 
-class ReserveData{
-    constructor(name,date,email,people,request){
-        this.name = name;
-        this.date = date;
-        this.email = email;
-        this.people = people;
-        this.request = request;
-    }
 
+function makeId() {
+    let result = '';
+    let num = Math.floor(Math.random() * 30) + 1;
+    let twoDigit = num.toString().padStart(2, '0');
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUV';
+    const charactersLength = characters.length;
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += twoDigit;
+    return result;
 }
 
 reserveForm.addEventListener('submit',(e)=>{
@@ -138,17 +152,45 @@ reserveForm.addEventListener('submit',(e)=>{
     const formValues = {
         name: formData.get('name'),
         date: formData.get('date'),
+        time: formData.get('time'),
         email: formData.get('email'),
         request: formData.get('specials'),
         people: formData.get('people')
     };
-    reserveContainer.innerHTML = '';
 
-    const order = document.createElement('h3')
-
-    order.textContent = `Spot Reserved for ${formValues['name'].toUpperCase}`
-
-    reserveContainer.appendChild(order)
-      
+    spotReserved(formValues)
 
 })
+
+function spotReserved(formValues){
+    contentContainer.innerHTML = '';
+    const orderDiv = document.createElement('div')
+    const resDetails = document.createElement('div')
+    const orderTitle = document.createElement('h1')
+    const order = document.createElement('h3')
+    const ul = document.createElement('ul')
+    let tableNumber = makeId()
+
+    orderDiv.id = 'order'
+    order.id = 'orderText'
+    resDetails.id = 'resDetails'
+
+    ul.className = 'r-list'
+    for (const [key, value] of Object.entries(formValues)) {
+        if (key){
+            const li = document.createElement('li');
+            li.textContent = `${key.charAt(0).toUpperCase() + key.slice(1)} - ${value}`;
+            ul.appendChild(li);
+        }
+    }
+
+
+    orderTitle.textContent = `Table ${tableNumber} Reserved for ${formValues['name'].toUpperCase()}`
+    order.textContent = 'Reservation Details'
+
+    orderDiv.appendChild(orderTitle)
+    resDetails.appendChild(order)
+    resDetails.appendChild(ul)
+    orderDiv.appendChild(resDetails)
+    contentContainer.appendChild(orderDiv)
+}
